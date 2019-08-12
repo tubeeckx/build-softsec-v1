e958a5dc5cb3a5b23e5c8016e3a55e8016a8af12
Jonas Tubeeckx <s103586@ap.be>
2019-08-12 11:47:42 +0200

    First Commit

---
+++ precache-manifest.c5e003bed5cd863b93fd4b34b7203318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec5d8e86e010f7871ee02b239d06f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5799d46af724ad1fc206",
+    "url": "/static/css/2.177fc970.chunk.css"
+  },
+  {
+    "revision": "d6ec2ccee121c5f70d6c",
+    "url": "/static/css/main.cd5f4718.chunk.css"
+  },
+  {
+    "revision": "5799d46af724ad1fc206",
+    "url": "/static/js/2.dcb06745.chunk.js"
+  },
+  {
+    "revision": "d6ec2ccee121c5f70d6c",
+    "url": "/static/js/main.390e6468.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
